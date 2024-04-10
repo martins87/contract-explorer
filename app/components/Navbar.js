e4e927310a9e9048e270cc2a9b4ab9971997e9d3
Daniel Martins <martinsda@gmail.com>
2024-04-10 18:47:44 -0300

    edit navbar and page styling

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,7 +5,17 @@ import Toolbar from "@mui/material/Toolbar";
 const Navbar = () => {
   return (
     <>
-      <AppBar position="static" sx={{ background: "white" }}>
+      <AppBar
+        sx={{
+          background: "#fff",
+          border: "1px solid #E9ECEF",
+          borderTop: 0,
+          borderLeft: 0,
+          borderRight0: 0,
+        }}
+        position="static"
+        elevation="0"
+      >
         <Toolbar sx={{ display: "flex", justifyContent: "flex-end" }}>
           <Button variant="outlined">Connect wallet</Button>
         </Toolbar>
--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
 // import styles from "./page.module.css";
 
+import Container from "@mui/material/Container";
+
 export default function Home() {
-  return <p>hello, world</p>;
+  return (
+    <Container sx={{ marginTop: 10 }} maxWidth="lg">
+      <div>Upload JSON ABI File</div>
+      <div>Contract Address</div>
+    </Container>
+  );
 }
