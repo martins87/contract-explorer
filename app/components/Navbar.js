d74067d58b23fb55483f650352a89b577e241b73
Daniel Martins <martinsda@gmail.com>
2024-04-16 02:54:25 -0300

    add logo

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,6 +4,8 @@ import { Fragment } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import { MetaMaskProvider } from "@metamask/sdk-react";
+import Image from "next/image";
+import Link from "next/link";
 
 import WalletConnect from "./WalletConnect";
 
@@ -33,7 +35,16 @@ const Navbar = () => {
         position="static"
         elevation={0}
       >
-        <Toolbar sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Link href="/">
+            <Image
+              sx={{ "&:hover": { cursor: "pointer" } }}
+              src="/sparq-logo.svg"
+              width={128}
+              height={48}
+              alt="sparq logo"
+            />
+          </Link>
           <MetaMaskProvider debug={false} sdkOptions={sdkOptions}>
             <WalletConnect />
           </MetaMaskProvider>
